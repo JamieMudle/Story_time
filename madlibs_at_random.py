6a1b4d73ece4d2bfa40456ee3bb1985aac0cd11e
Jamie <Jay.mudle@gmail.com>
2023-11-05 19:28:52 +0000

    JM: Updated random selection

--- madlibs_at_random.py
+++ madlibs_at_random.py
@@ -1,6 +1,15 @@
-from madlibs_games import vampires, spywords
+import os
 import random
 
-if __name__ == "__main__":
-    m = random.choice([vampires, spywords])
-    m.madlib()
+folder_name = "madlibs_games"
+folder_path = os.path.abspath(folder_name)
+file_list = [file for file in os.listdir(folder_path) if not file.startswith("__pycache__")]
+
+if len(file_list) >= 2:
+     selected_file = random.choice(file_list)
+     file_to_run = os.path.join(folder_path, selected_file)
+
+    
+     exec(open(file_to_run).read())
+else:
+    print("There are not enough files in the folder to make a selection.")
